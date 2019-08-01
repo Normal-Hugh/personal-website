d7b98d009ff7df23118a6c607251ac3a160b8554
Hugh <HBSmith5000@gmail.com>
2019-07-31 20:40:03 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,13 @@
 // }
 
 function add_image() {
-  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-  var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  if (isMobile()) {
+    var w = screen.width;
+    var h = screen.height;
+  } else {
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  }
   var src = "../Bee.JPG";
   show_bee("../Bee.JPG", w, (0.5*h), "Bee");
 }
@@ -22,3 +27,16 @@ function show_bee(src, width, height, alt) {
   
 }
 
+function isMobile() {
+  if( navigator.userAgent.match(/Android/i)
+  || navigator.userAgent.match(/webOS/i)
+  || navigator.userAgent.match(/iPhone/i)
+  || navigator.userAgent.match(/iPad/i)
+  || navigator.userAgent.match(/iPod/i)
+  || navigator.userAgent.match(/BlackBerry/i)
+  || navigator.userAgent.match(/Windows Phone/i)){
+    return true;
+  } else {
+    return false;
+  }
+}
