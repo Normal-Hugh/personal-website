23559c6265dadca5b76677cbd550724f4ef740ab
Hugh <HBSmith5000@gmail.com>
2019-07-31 19:55:05 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,18 @@
 //    alert("you clicked the button");
 // }
 
-function test_image(src, width, height, alt) {
-   var img = document.createElement("img");
-   img.src = src;
-   img.width = width;
-   img.height = height;
-   img.alt = alt;
-   document.body.appendChild(img);
+function add_google_logo() {
+  var src = "https://yt3.ggpht.com/a/AGF-l7-BBIcC888A2qYc3rB44rST01IEYDG3uzbU_A=s288-c-k-c0xffffffff-no-rj-mo";
+  show_image("https://yt3.ggpht.com/a/AGF-l7-BBIcC888A2qYc3rB44rST01IEYDG3uzbU_A=s288-c-k-c0xffffffff-no-rj-mo", 276,110, "Google Logo");
 }
+
+
+function show_image(src, width, height, alt) {
+  var img = document.createElement("img");
+  img.src = src;
+  img.width = width;
+  img.height = height;
+  img.alt = alt;
+  document.body.appendChild(img);
+}
+
