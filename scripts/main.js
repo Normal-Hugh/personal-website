ec81b24b5e36771528d049cadd2d944c5f3b759d
Hugh <HBSmith5000@gmail.com>
2019-08-01 14:58:29 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,34 +3,29 @@
 //    alert("you clicked the button");
 // }
 
-function add_image() {
-  if (isMobile()) {
-    var w = screen.width;
-    var h = screen.height;
-  } else {
-    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    if (w > 1000) {
-      w = 1000;
-    }
-    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    if (h > 1000) {
-      h = 1000;
-    }
-  }
-  var src = "../Bee.JPG";
-  show_bee("../Bee.JPG", w, h, "Bee");
+function add_image(file, name) {
+  var src = file;
+  var alt = name;
+  show_bee(src, w, h, alt);
 }
 //bee is 301W x 311H
 
-function show_bee(src, width, height, alt) {
-  
+function showBackgroundImage(src, width, height, alt) {
   var img = document.createElement("img");
   img.src = src;
   img.width = width;
   img.height = height;
   img.alt = alt;
   document.body.appendChild(img);
-  
+}
+
+function show_bee(src, width, height, alt) {
+  var img = document.createElement("img");
+  img.src = src;
+  img.width = width;
+  img.height = height;
+  img.alt = alt;
+  document.body.appendChild(img);
 }
 
 function isMobile() {
@@ -44,5 +39,27 @@ function isMobile() {
     return true;
   } else {
     return false;
+  }
+}
+
+function getWidth() {
+  if (isMobile()) {
+    var w = screen.width;
+  } else {
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    if (w > 800) {
+      w = 800;
+    }
+  }
+  return w;
+}
+function getHeight() {
+  if (isMobile()) {
+    var h = screen.height;
+  } else {
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    if (h > 800) {
+      h = 800;
+    }
   }
 }
