e900f01f98683f96c042a25d7601c5ddc6b6982e
Hugh <HBSmith5000@gmail.com>
2019-07-31 19:41:27 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,12 @@ let myButton = document.querySelector('button');
 myButton.onclick = function() {
    alert("you clicked the button");
 }
+
+function test_image(src, width, height, alt) {
+   var img = document.createElement("img");
+   img.src = src;
+   img.width = width;
+   img.height = height;
+   img.alt = alt;
+   document.body.appendChild(img);
+}
