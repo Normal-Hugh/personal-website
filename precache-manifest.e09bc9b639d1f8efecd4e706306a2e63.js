fabb52215ec885a4ad1c14b7f4112a7636728606
Hugh <HBSmith5000@gmail.com>
2020-05-06 19:01:22 -0700

    Updates

---
+++ precache-manifest.e09bc9b639d1f8efecd4e706306a2e63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6e632f7fc70a78cfbb13f0b05521fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea42ebe622e19bbd8256",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "48614f4fbfcc7bd6c24b",
+    "url": "/static/js/2.ae021ef8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ae021ef8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea42ebe622e19bbd8256",
+    "url": "/static/js/main.69b38722.chunk.js"
+  },
+  {
+    "revision": "9d7ec453320509f6d66c",
+    "url": "/static/js/runtime-main.42cfb24a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
